7bc914862269703827153513ab0566a79a483ed7
RuslanVoskresenskyi <vos.nau.04@gmail.com>
2024-12-26 19:08:49 +0200

    init shadcn and supabase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,33 @@
+import type { Metadata } from 'next'
+import './globals.css'
+import { Inter as FontSans } from 'next/font/google'
+import { cn } from '@/lib/utils'
+
+// Load the font
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
+
+export const metadata: Metadata = {
+  title: 'Chapter 1 Homework',
+  description: 'Chapter 1 homework reference',
+}
+
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang='en'>
       <body
+        className={cn(
+          'min-h-screen bg-gradient-to-r from-gray-100 via-gray-200 to-gray-300 text-gray-800 font-sans antialiased',
+          fontSans.variable
+        )}
       >
-        {children}
+        <div className='container mx-auto px-4'>
+          {children}
+        </div>
       </body>
     </html>
   )
